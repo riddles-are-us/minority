3298d1c58fc8da99a44a5f82ff57f5425aa1d3ca
xgao <xgao@zoyoe.com>
2025-05-19 01:10:09 +1000

    utilizing convention more

--- src/command.rs
+++ src/command.rs
@@ -1,79 +1,12 @@
 use zkwasm_rest_convention::IndexedObject;
-use zkwasm_rust_sdk::require;
-use zkwasm_rest_abi::WithdrawInfo;
 use zkwasm_rest_abi::enforce;
 use zkwasm_rest_convention::WithBalance;
+use zkwasm_rest_convention::{SubCommand, CommandHandler};
 use crate::history::RoundResult;
-use crate::settlement::SettlementInfo;
 use crate::player::GamePlayer;
 use crate::player::RoundInfo;
 use crate::state::GLOBAL_STATE;
 use crate::error::*;
-
-#[derive (Clone)]
-pub enum Command {
-    // standard activities
-    Activity(Activity),
-    // standard withdraw and deposit
-    Withdraw(Withdraw),
-    Deposit(Deposit),
-    // standard player install and timer
-    InstallPlayer,
-    Tick,
-}
-
-
-pub trait CommandHandler {
-    fn handle(&self, pid: &[u64; 2], nonce: u64, rand: &[u64; 4], counter: u64) -> Result<(), u32>;
-}
-
-#[derive (Clone)]
-pub struct Withdraw {
-    pub data: [u64; 3],
-}
-
-impl CommandHandler for Withdraw {
-    fn handle(&self, pid: &[u64; 2], nonce: u64, _rand: &[u64; 4], _counter: u64) -> Result<(), u32> {
-        let mut player = GamePlayer::get_from_pid(pid);
-        match player.as_mut() {
-            None => Err(ERROR_PLAYER_NOT_EXIST),
-            Some(player) => {
-                player.check_and_inc_nonce(nonce);
-                let balance = player.data.balance;
-                let amount = self.data[0] & 0xffffffff;
-                unsafe { require(balance >= amount) };
-                player.data.balance -= amount;
-                let withdrawinfo =
-                    WithdrawInfo::new(&[self.data[0], self.data[1], self.data[2]], 0);
-                SettlementInfo::append_settlement(withdrawinfo);
-                player.store();
-                Ok(())
-            }
-        }
-    }
-}
-
-#[derive (Clone)]
-pub struct Deposit {
-    pub data: [u64; 3],
-}
-
-impl CommandHandler for Deposit {
-    fn handle(&self, pid: &[u64; 2], nonce: u64, _rand: &[u64; 4], _counter: u64) -> Result<(), u32> {
-        let mut admin = GamePlayer::get_from_pid(pid).unwrap();
-        admin.check_and_inc_nonce(nonce);
-        let mut player = GamePlayer::get_from_pid(&[self.data[0], self.data[1]]);
-        match player.as_mut() {
-            None => Err(ERROR_PLAYER_NOT_EXIST),
-            Some(player) => {
-                player.data.balance += self.data[2];
-                player.store();
-                admin.store();
-                Ok(())
-            }
-        }
-    }
-}
 
 #[derive (Clone)]
 pub enum Activity {
@@ -83,9 +16,27 @@ pub enum Activity {
     Settle,
 }
 
+const BUY_CARD: u64 = 4;
+const CLAIM_REWARD: u64 = 5;
+const SETTLE: u64 = 6;
+
+impl SubCommand for Activity {
+    fn decode(command: u64, params: &[u64]) -> Option<Self> {
+        if command == BUY_CARD {
+            Some(Activity::Buy(params[0], params[1]))
+        } else if command == CLAIM_REWARD {
+            Some(Activity::Claim(params[9]))
+        } else if command == SETTLE {
+            Some(Activity::Settle)
+        } else {
+            None
+        }
+    }
+}
+
 
 impl CommandHandler for Activity {
-    fn handle(&self, pid: &[u64; 2], nonce: u64, _rand: &[u64; 4], counter: u64) -> Result<(), u32> {
+    fn handle<PlayerData>(&self, pid: &[u64; 2], nonce: u64, _rand: &[u64; 4], counter: u64) -> Result<(), u32> {
         let mut player = GamePlayer::get_from_pid(pid);
         match player.as_mut() {
             None => Err(ERROR_PLAYER_NOT_EXIST),
--- src/state.rs
+++ src/state.rs
@@ -1,21 +1,19 @@
 use crate::config::ADMIN_PUBKEY;
 use crate::player::{Owner, GamePlayer};
 use crate::settlement::SettlementInfo;
-use crate::Player;
-use core::slice::IterMut;
+use crate::player::PlayerData;
 use serde::Serialize;
 use std::cell::RefCell;
 use zkwasm_rest_abi::MERKLE_MAP;
 use zkwasm_rust_sdk::require;
 use zkwasm_rest_abi::enforce;
-use crate::command::Command;
 use crate::command::Activity;
-use crate::command::Deposit;
-use crate::command::Withdraw;
-use crate::command::CommandHandler;
 use crate::history::RoundResult;
 use zkwasm_rest_convention::IndexedObject;
+use zkwasm_rest_convention::TransactionData;
 use zkwasm_rest_convention::clear_events;
+use zkwasm_rest_convention::Command;
+use zkwasm_rest_convention::CommandHandler;
 use crate::error::*;
 
 
@@ -37,13 +35,6 @@ pub struct QueryState {
     cards: Vec<u64>,
 }
 
-const TICK: u64 = 0;
-const INSTALL_PLAYER: u64 = 1;
-const WITHDRAW: u64 = 2;
-const DEPOSIT: u64 = 3;
-const BUY_CARD: u64 = 4;
-const CLAIM_REWARD: u64 = 5;
-const SETTLE: u64 = 6;
 
 impl GlobalState {
     pub fn new() -> Self {
@@ -142,10 +133,7 @@ lazy_static::lazy_static! {
     pub static ref GLOBAL_STATE: SafeState = SafeState(RefCell::new(GlobalState::new()));
 }
 
-pub struct Transaction {
-    command: Command,
-    nonce: u64,
-}
+pub struct Transaction (TransactionData<Activity>);
 
 impl Transaction {
     pub fn decode_error(e: u32) -> &'static str {
@@ -153,50 +141,8 @@ impl Transaction {
     }
 
     pub fn decode(params: &[u64]) -> Self {
-        let command = params[0] & 0xff;
-        let nonce = params[0] >> 16;
-        //zkwasm_rust_sdk::dbg!("command is {}\n", command); // only token index 0 is supported
-        //zkwasm_rust_sdk::dbg!("nonce is {}\n", nonce); // only token index 0 is supported
-        let command = if command == WITHDRAW {
-            Command::Withdraw (Withdraw {
-                data: [params[2], params[3], params[4]]
-            })
-        } else if command == DEPOSIT {
-            enforce(params[3] == 0, "check deposit index"); // only token index 0 is supported
-            Command::Deposit (Deposit {
-                data: [params[1], params[2], params[4]]
-            })
-        } else if command == INSTALL_PLAYER {
-            Command::InstallPlayer
-        } else if command == BUY_CARD {
-            Command::Activity (Activity::Buy(params[1], params[2]))
-        } else if command == CLAIM_REWARD {
-            Command::Activity (Activity::Claim(params[1]))
-        } else if command == SETTLE {
-            Command::Activity (Activity::Settle)
-        } else {
-            unsafe {zkwasm_rust_sdk::require(command == TICK)};
-            Command::Tick
-        };
-        Transaction {
-            command,
-            nonce,
-        }
-    }
-
-    pub fn create_player(&self, pkey: &[u64; 4]) -> Result<(), u32> {
-        let round = GLOBAL_STATE.0.borrow().round;
-        let player = GamePlayer::get(pkey);
-        match player {
-            Some(_) => Err(ERROR_PLAYER_ALREADY_EXIST),
-            None => {
-                let mut player = Player::new(pkey);
-                player.data.balance = 100000;
-                player.data.round = round;
-                player.store();
-                Ok(())
-            }
-        }
+        let tdata = TransactionData::<Activity>::decode(params);
+        Transaction (tdata)
     }
 
     pub fn tick(&self) {
@@ -212,32 +158,44 @@ impl Transaction {
         }
     }
 
+    pub fn create_player(pkey: &[u64; 4]) -> Result<(), u32> {
+        let round = GLOBAL_STATE.0.borrow().round;
+        let player = GamePlayer::get(pkey);
+        match player {
+            Some(_) => Err(ERROR_PLAYER_ALREADY_EXIST),
+            None => {
+                let mut player = GamePlayer::new(pkey);
+                player.data.balance = 100000;
+                player.data.round = round;
+                player.store();
+                Ok(())
+           }
+        }
+    }
+
+
     pub fn process(&self, pkey: &[u64; 4], rand: &[u64; 4]) -> Vec<u64> {
         let pid = GamePlayer::pkey_to_pid(&pkey);
         let counter = GLOBAL_STATE.0.borrow().counter;
-        let e = match &self.command {
+        let e = match &self.0.command {
             Command::Tick => {
                 unsafe { require(*pkey == *ADMIN_PUBKEY) };
                 self.tick();
                 0
             },
-            Command::InstallPlayer => self.create_player(pkey)
+            Command::InstallPlayer => Self::create_player(pkey)
                 .map_or_else(|e| e, |_| 0),
-            Command::Withdraw(cmd) => cmd.handle(&pid, self.nonce, rand, counter)
-                .map_or_else(|e| e, |_| 0),
-            Command::Activity(cmd) => cmd.handle(&pid, self.nonce, rand, counter)
+            Command::Withdraw(cmd) => cmd.handle::<PlayerData>(&pid, self.0.nonce, rand, counter)
                 .map_or_else(|e| e, |_| 0),
             Command::Deposit(cmd) => {
                 unsafe { require(*pkey == *ADMIN_PUBKEY) };
-                cmd.handle(&pid, self.nonce, rand, counter)
+                cmd.handle::<PlayerData>(&pid, self.0.nonce, rand, counter)
                     .map_or_else(|e| e, |_| 0)
             },
+            Command::Activity(cmd) => cmd.handle::<PlayerData>(&pid, self.0.nonce, rand, counter)
+                .map_or_else(|e| e, |_| 0),
         };
         let round = GLOBAL_STATE.0.borrow().round;
-        zkwasm_rust_sdk::dbg!("events before {:?}\n", {unsafe {&zkwasm_rest_convention::EVENTS}});
-        let events = clear_events(vec![e as u64, round]);
-        zkwasm_rust_sdk::dbg!("events {:?}\n", {unsafe {&zkwasm_rest_convention::EVENTS}});
-        zkwasm_rust_sdk::dbg!("events {:?}\n", {&events});
-        events
+        clear_events(vec![e as u64, round])
     }
 }
